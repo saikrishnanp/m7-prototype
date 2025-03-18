9116c211e936ae818159b7a15b45f1952506f8fc
Saikrishnan P. <saikrishnan.perumal@solitontech.local>
2025-03-18 23:27:09 +0530

    tryout tailwind css
    Signed-off-by: Saikrishnan P. <saikrishnan.perumal@solitontech.local>

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,4 +1,4 @@
-// import styles from './TestComponent.module.scss';
+import styles from "./TestComponent.module.scss";
 
 import { useSelector } from "react-redux";
 import { RootState } from "src/redux/store";
@@ -6,6 +6,19 @@ import { RootState } from "src/redux/store";
 export const TestComponent = () => {
   const count = useSelector((state: RootState) => state.counter.value);
 
-  console.log(count, 'render');
-  return <div>test count! - {count}</div>;
+  console.log(count, "render");
+  return (
+    <>
+      <div
+        className={`font-bold underline shadow-xl shadow-fuchsia-400/95 ${styles.countContainer} dark:bg-amber-200 md:flex sm:grid space-x-4`}
+      >
+        test count: {count}
+      </div>
+      <div className="space-y-10 flex flex-col items-center justify-center">
+        <button className="divide-zinc-300 w-fit">B1</button>
+        <button className="divide-zinc-300 w-fit">B1</button>
+        <button className="divide-zinc-300 w-fit">B1</button>
+      </div>
+    </>
+  );
 };
